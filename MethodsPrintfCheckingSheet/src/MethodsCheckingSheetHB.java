ed38cc6ef9df6581b4f917c2cfa49bf4cba5920d
Bijayananda <S02712554>
2021-10-21 10:54:05 -0600

    Methods Checking Sheet

--- MethodsPrintfCheckingSheet/src/MethodsCheckingSheetHB.java
+++ MethodsPrintfCheckingSheet/src/MethodsCheckingSheetHB.java
@@ -7,13 +7,26 @@ public class MethodsCheckingSheetHB
 	public static void main(String[] args) 
 	{
 		input = new Scanner(System.in);
+		int num; // LCV
+		num = 0;
+		int high;
+		int low;
+		high = 1003;
+		low = 9;
 		Random randGen; //Random number generator
 		randGen = new Random();
 		int answer;	// Guessed random number
-		answer = randGen.nextInt(9-1003);
+		System.out.printf("%63s %n", "random table");
+		while (num < 70)
+		{
+			answer = randGen.nextInt(high-low) + low;
+			num++;
+			System.out.printf("%8d", answer);
+			if (num % 7 == 0)
+			{
+				System.out.printf("%n");
+			}
+		} // end of while loop
 		
-		
-
-	}
-
-}
+	} // end of main
+} // end of class
